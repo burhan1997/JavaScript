051948c4a5a74398891562cb60c9a95f70b4bd3f
Burhan Elaldi <burhanelaldi97@gmail.com>
2023-11-10 13:31:58 +0100

    prep exercises from week2 is completed

--- Week2/prep-exercises/1-traffic-light/traffic-light.js
+++ Week2/prep-exercises/1-traffic-light/traffic-light.js
@@ -7,12 +7,18 @@
 
 function getCurrentState(trafficLight) {
   // TODO
+  return trafficLight.possibleStates[trafficLight.stateIndex];
   // Should return the current state (i.e. colour) of the `trafficLight`
   // object passed as a parameter.
 }
 
 function getNextStateIndex(trafficLight) {
   // TODO
+  if(trafficLight.stateIndex == 2){
+    return 0;
+  }else{
+    return (trafficLight.stateIndex = trafficLight.stateIndex + 1);
+  }
   // Return the index of the next state of the `trafficLight` such that:
   // - if the color is green, it will turn to orange
   // - if the color is orange, it will turn to red
